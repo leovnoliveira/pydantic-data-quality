90a4b02c51973a8da75fe08c23fc8351eec96ecd
leovnoliveira <consultoria1.ceo@gmail.com>
2025-10-15 10:01:00 -0300

    etl: inicio da construção da função de etl

---
+++ app/etl.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from pathlib import Path
+
+import os
+
+def load_settings():
+    """Carrega as configurações a partir de variáveis de ambiente"""
+
+    dotenv_path = Path.cwd() / '.env'
+    load_dotenv(dotenv_path=dotenv_path)
+
+    settings = {
+        "db_host": os.getenv("POSTGRES_HOST"),
+        "db_port": os.getenv("POSTGRES_PORT"),
+        "db_name": os.getenv("POSTGRES_DB"),
+        "db_user": os.getenv("POSTGRES_USER"),
+        "db_password": os.getenv("POSTGRES_PASSWORD"),
+    }
+
+    return settings
+
+
+def extrair_do_sql(query: str) -> pd.DataFrame:
+    """Extrai dados de um banco de dados SQL e retorna um DataFrame do pandas"""
+
+    settings = load_settings()
+    engine = create_engine(
+        f"postgresql+psycopg2://{settings['db_user']}:{settings['db_password']}@{settings['db_host']}:{settings['db_port']}/{settings['db_name']}"
+    )
+
+    with engine.connect() as connection, connection.begin():
+        df = pd.read_sql_query(query, connection)
+
+    return df
+
+
+if __name__ == "__main__":
+    query = "SELECT * FROM produtos_bronze"
+    df = extrair_do_sql(query)
+    print(df.head())
